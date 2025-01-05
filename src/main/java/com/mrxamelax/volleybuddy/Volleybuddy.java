5ace83de3cddfb13e092127fc4cefaa4af82c431
Theo Stötzer <hallo>
2025-01-06 00:01:13 +0100

    Added project files

---
+++ src/main/java/com/mrxamelax/volleybuddy/Volleybuddy.java
@@ -0,0 +1,35 @@
+package com.mrxamelax.volleybuddy;
+
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+import reactor.core.publisher.Mono;
+
+public class Volleybuddy {
+
+    public static void main(String[] args) {
+        
+
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
+                gateway.on(MessageCreateEvent.class, event -> {
+                    Message message = event.getMessage();
+
+                    if (message.getContent().equalsIgnoreCase("!ping")) {
+                        return message.getChannel()
+                                .flatMap(channel -> channel.createMessage("pong!"));
+                    }
+
+
+
+                    return Mono.empty();
+                })
+        );
+
+        login.block();
+
+    }
+
+}
